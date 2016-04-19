4fef6d624f0dd8e58e2cdac83532ab3e31a16b02
Advait Patel <advaitpa93@gmail.com>
2016-04-19 13:30:04 -0500

    Added validations on fields

---
+++ CampusConnect/CampusConnect-ejb/src/main/java/se/project/campusconnect/persistence/StudentTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.project.campusconnect.persistence;
+import javax.ejb.BeforeCompletion;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+/**
+ *
+ * @author Advait
+ */
+public class StudentTest 
+{
+    
+}
